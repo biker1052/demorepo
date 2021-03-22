599bd74fb9bf1d199572c32c2dd922e8d769dbdc
biker1052 <uirchenkoden0303@rambler.ru>
2021-03-22 23:01:29 +0600

    Bubble sort add

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,21 @@
+
+fun bubbleSort(arr:IntArray):IntArray{
+    var swap = true
+    while(swap){
+        swap = false
+        for(i in 0 until arr.size-1){
+            if(arr[i] > arr[i+1]){
+                val temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i + 1] = temp
+
+                swap = true
+            }
+        }
+    }
+    return arr
+}
 fun main(){
-    println("My first commit")
-    println("My sec commit")
+    val numbers = bubbleSort(intArrayOf(10, 2, 10, 3, 1, 2, 5, 2,-1, 3))
+    for (k in numbers) print("$k ")
 }
