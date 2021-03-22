c24ec06a4289474a341659ae861224ad5bf6b7c9
biker1052 <71536420+biker1052@users.noreply.github.com>
2021-03-23 01:43:53 +0600

    delete commits

--- src/main.kt
+++ src/main.kt
@@ -19,12 +19,5 @@ fun main(){
     val numbers = bubbleSort(intArrayOf(10, 2, 10, 3, 1, 2, 5, 2,-1, 3))
     for (k in numbers) print("$k ")
 
-    println("3 commit")
-    println("4 commit")
-
-    println("5 commit")
-    println("6 commit")
-
-    println("7 commit")
 
 }
